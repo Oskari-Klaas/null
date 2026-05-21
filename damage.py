0de8a9fc938f6e2d04b6722fd9ff8c0c4994e8c7
Renald Mattias <r.kronbergs@voco.ee>
2026-05-21 10:52:21 +0300

    Made the game playable and added choosing state as well as two new enemies.

--- damage.py
+++ damage.py
@@ -1,8 +1,6 @@
 import pygame
 
 def process_collision(player, enemy):
-    # If player is still alive and enemy is dashing...
-    if player.alive and enemy.state == "dashing":
-        # Check for the hit
+    if player.alive and enemy.state == "active":
         if player.rect.colliderect(enemy.rect):
             player.die()
